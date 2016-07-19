f3b17fe2402f3010c4b88e018c0c2225d601538a
Gyowanny Queiroz <gyowanny@gmail.com>
2016-07-19 11:22:28 -0300

    [ANYMARKET][2600] - Criado o modulo de callback para integracao com o anymarket

---
+++ anymarket-sdk-callback/src/main/java/br/com/anymarket/sdk/callback/dto/Callback.java
@@ -0,0 +1,48 @@
+package br.com.anymarket.sdk.callback.dto;
+
+import br.com.anymarket.sdk.AnymarketPojo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+
+/**
+ * Created by gyowannyqueiroz on 7/19/16.
+ */
+public class Callback implements AnymarketPojo {
+
+    private String id;
+
+    @JsonProperty("name")
+    private String url;
+
+    public Callback() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String getPathURI() {
+        return "/callback";
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("url", url)
+                .toString();
+    }
+}
